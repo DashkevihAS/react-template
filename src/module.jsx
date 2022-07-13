d5105af052119a6e2f4bc3a4523a87c9fdcb4d0b
DashkevichAS <antonsnov0604@gmail.com>
2022-07-13 13:44:42 +0300

    create

---
+++ src/module.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import picture from "./img/mouse.jpg";
+
+const Module = () => {
+    <>
+        <h1>Hello, React</h1>
+        <img src={picture} alt="мышка" />
+    </>
+}
+export default Module;
